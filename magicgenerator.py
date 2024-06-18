08e9e10858b1a3b05de72de84e635ac6413b1a87
fm-grid <fmaciejak@griddynamics.com>
2024-06-18 14:56:39 +0200

    refactor

--- magicgenerator.py
+++ magicgenerator.py
@@ -1,93 +1,10 @@
-from argparse import ArgumentParser, Namespace
+from argparse import Namespace
+import cli
 from concurrent.futures import ThreadPoolExecutor
-import generator
 import json
 import os
 import random
-import sys
 import uuid
-
-
-def create_parser() -> ArgumentParser:
-    parser = ArgumentParser()
-    parser.add_argument('-o', '--output',
-        type=str,
-        default='.',
-        help='directory to which all generated files will be saved, default: current working directory'
-    )
-    parser.add_argument('-c', '--count',
-        type=int,
-        default=1,
-        help='number of json files to generate, default: 1'
-    )
-    parser.add_argument('-f', '--filename',
-        type=str,
-        help='base file name, when generating multiplie files it will be expanded by an affix, do not specify the extension',
-        required=True
-    )
-    parser.add_argument('-a', '--affix',
-        choices=['count', 'random', 'uuid'],
-        default='count',
-        help='file name affix, required when generating multiple files, options: count (default), random, uuid'
-    )
-    parser.add_argument('-s', '--schema',
-        type=str,
-        help='inline json schema or path to a file containing one',
-        required=True
-    )
-    parser.add_argument('-l', '--lines',
-        type=int,
-        default=1000,
-        help='number of lines each json file will contain, default: 1000'
-    )
-    parser.add_argument('--clear-path',
-        action='store_true',
-        help='clear the directory of all the files with the same base file name before generating new ones'
-    )
-    parser.add_argument('-p', '--processes',
-        type=int,
-        default=1,
-        help='number of processes that will be used to generate files'
-    )
-    return parser
-
-
-def load_schema(schema: str) -> dict[str, str]:
-    json_string: str
-    if '{' in schema: # inline schema
-        json_string = schema
-    else: # path to a schema file
-        with open(schema, 'r') as file:
-            json_string = file.read()
-    return json.loads(json_string)
-
-
-def parse_arguments(parser: ArgumentParser, args: list[str]) -> Namespace:
-    namespace = parser.parse_args(args)
-
-    # count
-    if namespace.count < 0:
-        parser.error(f'argument -c/--count: invalid non-negative int value: {namespace.count}')
-    
-    # schema
-    schema_generator: generator.SchemaGenerator
-    try:
-        schema = load_schema(namespace.schema)
-        schema_generator = generator.SchemaGenerator(schema)
-    except ValueError:
-        parser.error(f'argument -s/--schema: invalid schema')
-    namespace.generator = schema_generator
-    
-    # lines
-    if namespace.lines <= 0:
-        parser.error(f'argument -l/--lines: invalid positive int value: {namespace.lines}')
-    
-    # processes
-    if namespace.processes <= 0:
-        parser.error(f'argument -p/--processes: invalid positive int value: {namespace.processes}')
-    namespace.processes = min(namespace.processes, os.cpu_count())
-    
-    return namespace
 
 
 def generate_affixes(type: str, count: int) -> list[str]:
@@ -175,8 +92,7 @@ def main_generate_files(namespace: Namespace) -> None:
 
 
 def main():
-    parser = create_parser()
-    namespace = parse_arguments(parser, sys.argv[1:])
+    namespace = cli.get_arguments()
 
     match namespace.count:
         case 0:
