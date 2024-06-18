14021fee8076020d9a5830af7711b5ac31e195e0
fm-grid <fmaciejak@griddynamics.com>
2024-06-18 14:23:54 +0200

    generating files

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from argparse import ArgumentParser, Namespace
+from concurrent.futures import ThreadPoolExecutor
 import generator
 import json
 import os
+import random
 import sys
+import uuid
 
 
 def create_parser() -> ArgumentParser:
@@ -87,10 +90,80 @@ def parse_arguments(parser: ArgumentParser, args: list[str]) -> Namespace:
     return namespace
 
 
+def generate_affixes(type: str, count: int) -> list[str]:
+    match type:
+        case 'count':
+            digits = len(str(count - 1))
+            return [f'{i:0{digits}d}' for i in range(count)]
+    
+        case 'random':
+            values = set()
+            while len(values) < count:
+                values.add(random.randbytes(16).hex())
+            return list(values)
+        
+        case 'uuid':
+            values = set()
+            while len(values) < count:
+                values.add(str(uuid.uuid4()))
+            return list(values)
+
+
+def list_split(list: list, n: int) -> list[list]:
+    sublists = [[] for _ in range(n)]
+    for index, item in enumerate(list):
+        sublists[index % n].append(item)
+    return sublists
+
+
+def generate_line(namespace: Namespace) -> str:
+    data = namespace.generator.get()
+    line = json.dumps(data)
+    return line
+
+
+def generate_lines(namespace: Namespace, lines: int = None) -> list[str]:
+    if lines is None:
+        lines = namespace.lines
+    return [generate_line(namespace) for _ in range(lines)]
+
+
+def generate_file(namespace: Namespace, affix: str):
+    filename = namespace.filename + affix + '.jsonl'
+    path = namespace.output + '/' + filename
+    with open(path, 'w') as file:
+        file.write('\n'.join(generate_lines(namespace)))
+
+
+def generate_files(namespace: Namespace, affixes: list[str]):
+    for affix in affixes:
+        generate_file(namespace, affix)
+
+
+def main_stdout(namespace: Namespace):
+    for _ in range(namespace.lines):
+        print(generate_line(namespace))
+
+def main_generate_files(namespace: Namespace):
+    if not os.path.exists(namespace.output):
+        os.mkdir(namespace.output)
+    affixes = generate_affixes(namespace.affix, namespace.count)
+    affix_lists = list_split(affixes, namespace.processes)
+    with ThreadPoolExecutor(max_workers=namespace.processes) as executor:
+        func = lambda affixes: generate_files(namespace, affixes)
+        executor.map(func, affix_lists)
+
+
 def main():
     parser = create_parser()
     namespace = parse_arguments(parser, sys.argv[1:])
-    print(namespace)
+
+    match namespace.count:
+        case 0:
+            main_stdout(namespace)
+
+        case _:
+            main_generate_files(namespace)
 
 
 if __name__ == '__main__':
